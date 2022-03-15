97f10ec67ef335f2d2741fb63d695dfe5bd74c91
Antonio <tonioufa@gmail.com>
2022-03-15 11:41:21 +0000

    with remapping

--- foundry-demo/src/Contract.sol
+++ foundry-demo/src/Contract.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
-import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+// import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "openzeppelin-erc20/ERC20.sol";
 
 contract Contract is ERC20 {
     constructor() ERC20("MyToken", "MTKN") {
